366c63034e7ebf2a4e9938508355c2b83a2aabdb
hankaskuhrovcova <hana.skuhrovcova@gmail.com>
2026-03-09 23:42:17 +0000

    404 page started

--- test_root/pages/views.py
+++ test_root/pages/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from . models import Page
+from django.shortcuts import render, get_object_or_404
+from .models import Page
 
 def index(request, pagename=''):
     pagename = '/' + pagename
-    pg = Page.objects.get(permalink=pagename)
+    pg = get_object_or_404(Page, permalink=pagename)
     context = {
         'title': pg.title,
-        'content': pg.content
+        'content': pg.content,
+        'page_list': Page.objects.all(),
     }
-    return render(request, 'base.html', context)
+    return render(request, 'pages/page.html', context)
--- test_root/test_site/settings.py
+++ test_root/test_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rf8#7prb2dh43+60)y1lkv3!a1ts#t@_@n%m-+-ip_t8%oi7&w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
