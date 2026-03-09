56df48d11932c991c5f47f84973af4026c7c2866
Jamie O'Reilly <jamie.oreilly55@mail.dcu.ie>
2026-03-09 16:27:37 +0000

    got home page working

--- test_root/pages/models.py
+++ test_root/pages/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Page(models.Model):
-    title = models.CharField(max_length=60)
-    permalink = models.CharField(max_length=12, unique=True)
-    bodytext = models.TextField('Page Content', blank=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
