208ec8e3eea7c169efcf7be4d00d50eee7f8da4f
Anastasiya . <anastasiya.kunitska@gmail.com>
2026-03-09 20:06:29 +0000

    Page layout and css

--- test_root/pages/models.py
+++ test_root/pages/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Page(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    permalink = models.CharField(max_length=12, unique=True)
     content = models.TextField()
 
     def __str__(self):
--- test_root/pages/views.py
+++ test_root/pages/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from . models import Page
 
-def index(request):
-    return render(request, "pages/index.html")
+def index(request, pagename=''):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.content
+    }
+    return render(request, 'base.html', context)
